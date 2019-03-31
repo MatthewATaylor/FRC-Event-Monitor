e52434cdd4abd0c760dee21bb1194f1130d46172
Matthew Taylor <MatthewAlanTaylor@outlook.com>
2019-03-31 15:13:07 -0400

    Add team number and next match number

--- FRC Event Monitor/FRC Event Monitor.py
+++ FRC Event Monitor/FRC Event Monitor.py
@@ -9,6 +9,7 @@ timeBarColor = [0, 0, 0]
 previousSlideMillisTime = 0
 sponsorsIndex = 0
 isRunning = True
+matchNumber = 0
 
 pygame.init()
 
@@ -20,15 +21,23 @@ window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 pygame.display.set_caption("FRC Pit Monitor")
 
 clock = pygame.time.Clock()
-timeFont = pygame.font.SysFont("arial", int(0.06 * SCREEN_WIDTH))
+timeFont = pygame.font.SysFont("arial", int(0.035 * SCREEN_WIDTH))
+matchFont = pygame.font.SysFont("arial", int(0.035 * SCREEN_WIDTH))
 sponsorFont = pygame.font.SysFont("arial", int(0.03 * SCREEN_WIDTH))
+teamNumFont = pygame.font.SysFont("arial", int(0.04 * SCREEN_WIDTH))
+teamLogoWidth = 0.21 * SCREEN_WIDTH
 teamLogo = pygame.image.load("Resources/Hudson Hybrids Logo.png")
-teamLogo = pygame.transform.scale(teamLogo, (int(0.21 * SCREEN_WIDTH), int(0.37 * SCREEN_HEIGHT)))
+teamLogo = pygame.transform.scale(teamLogo, (int(teamLogoWidth), int(0.37 * SCREEN_HEIGHT)))
 teamLogo = teamLogo.convert_alpha()
 
 def setEventParameters():
     global eventTime
+    global matchNumber
+
     pygame.display.quit()
+
+    print("Next Match Number: ", end = "")
+    matchNumber = input()
 
     print("Event Hour: ", end = "")
     eventTime[0] = input()
@@ -59,7 +68,6 @@ def getCurrentTime():
     return currentTime
 
 def displayTime():
-    global eventTime
     deltaTime = [0, 0, 0]
     currentTime = getCurrentTime()
 
@@ -78,11 +86,10 @@ def displayTime():
     for i in range(0, 3):
         deltaTime[i] = str(deltaTime[i])
 
-    global timeFont
     for i in range(0, 3):
         if len(deltaTime[i]) < 2:
             deltaTime[i] = "0" + deltaTime[i]
-    eventTimeString = "Time to Next Event: " + deltaTime[0] + ":" + deltaTime[1] + ":" + deltaTime[2]
+    eventTimeString = "Time to Next Match: " + deltaTime[0] + ":" + deltaTime[1] + ":" + deltaTime[2]
     eventTimeTextSurface = timeFont.render(eventTimeString, True, (0, 0, 0))
 
     eventTimeTextDimensions = {
@@ -90,27 +97,32 @@ def displayTime():
         "height": timeFont.size(eventTimeString)[1]
     }
 
-    global SCREEN_WIDTH
-    global TIME_BAR_HEIGHT
     eventTimeTextPos = {
-        "x": SCREEN_WIDTH / 2 - eventTimeTextDimensions["width"] / 2,
+        "x": SCREEN_WIDTH * 10 / 1920,
         "y": TIME_BAR_HEIGHT / 2 - eventTimeTextDimensions["height"] / 2
     }
 
-    global window
     window.blit(eventTimeTextSurface, (eventTimeTextPos["x"], eventTimeTextPos["y"]))
 
+def displayNextMatch():
+    nextMatchNumberString = "Next Match Number: " + str(matchNumber)
+    nextMatchNumberTextSurface = matchFont.render(nextMatchNumberString, True, (0, 0, 0))
+    eventTimeTextWidth = timeFont.size("Time to Next Match: 00:00:00")[0]
+    textX = eventTimeTextWidth + SCREEN_WIDTH * 150 / 1920
+    textY = TIME_BAR_HEIGHT / 2 - matchFont.size(nextMatchNumberString)[1] / 2
+
+    window.blit(nextMatchNumberTextSurface, (textX, textY))
+
 def displaySponsors():
-    global SCREEN_HEIGHT
     SPONSOR_TEXT_Y = 0.44 * SCREEN_HEIGHT
     SPONSOR_TEXT_HEIGHT = 0.11 * SCREEN_HEIGHT
 
-    sponsors = ["Acme of Hudson", "NASA Robotics Alliance Project Registration Grant", "Burton D. Morgan Foundation Extracurricular Support Grant", "Thomas and Tracy Corpus", "Jeffrey and Angela Gotthardt", "Suzanne and Paul Westlake", "Ivo and Elizabeth Cavoili"]
+    sponsors = ["Acme of Hudson", "NASA Robotics Alliance Project Registration Grant", "Burton D. Morgan Foundation Extracurricular Support Grant", "Thomas and Tracy Corpus", "Jeffrey and Angela Gotthardt", "Suzanne and Paul Westlake", "Ivo and Elizabeth Cavoili", "Kathleen Owen Russell", "Firas and Melinda Al-Ali"]
 
     global previousSlideMillisTime
     currentMillisTime = time.time()
     deltaTime = currentMillisTime - previousSlideMillisTime
-    
+
     global sponsorsIndex
     if deltaTime > 2:
         previousSlideMillisTime = currentMillisTime
@@ -119,14 +131,17 @@ def displaySponsors():
         else:
             sponsorsIndex += 1
 
-    global sponsorFont
     sponsorString = "Sponsors: " + sponsors[sponsorsIndex]
     sponsorTextSurface = sponsorFont.render(sponsorString, True, (0, 0, 0))
 
-    global window
-    global SCREEN_WIDTH
     pygame.draw.rect(window, (50, 50, 120), pygame.Rect(0, SPONSOR_TEXT_Y, SCREEN_WIDTH, SPONSOR_TEXT_HEIGHT))
     window.blit(sponsorTextSurface, (5, SPONSOR_TEXT_Y + SPONSOR_TEXT_HEIGHT / 2 - sponsorFont.size(sponsorString)[1] / 2))
+
+def displayTeamNumber():
+    teamNumString = "Team 7486"
+    teamNumTextSurface = teamNumFont.render(teamNumString, True, (0, 0, 0))
+
+    window.blit(teamNumTextSurface, (SCREEN_WIDTH / 2 + SCREEN_WIDTH * 80 / 1920, 0.6 * SCREEN_HEIGHT + 80))
 
 while isRunning:
     for event in pygame.event.get():
@@ -140,8 +155,10 @@ while isRunning:
     pygame.draw.rect(window, (timeBarColor[0], timeBarColor[1], timeBarColor[2]), pygame.Rect(0, 0, SCREEN_WIDTH, TIME_BAR_HEIGHT))
     pygame.draw.rect(window, (100, 100, 125), pygame.Rect(0, TIME_BAR_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT - TIME_BAR_HEIGHT))
     pygame.draw.rect(window, (0, 0, 0), pygame.Rect(0, TIME_BAR_HEIGHT, SCREEN_WIDTH, TIME_BAR_HEIGHT / 4))
-    window.blit(teamLogo, (SCREEN_WIDTH / 2 - teamLogo.get_width() / 2, 0.6 * SCREEN_HEIGHT))
+    window.blit(teamLogo, (SCREEN_WIDTH / 2 - SCREEN_WIDTH * 90 / 1920 - teamLogoWidth, 0.6 * SCREEN_HEIGHT))
+    displayTeamNumber()
     displayTime()
+    displayNextMatch()
     displaySponsors()
     pygame.display.flip()
     
